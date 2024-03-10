4c2cfba30cd3567493943d767e12c1e81ee22f80
Jiao <jiaojiaobigbang@gmail.com>
2024-03-10 13:48:19 +1300

    register function update

--- server.js
+++ server.js
@@ -1,66 +1,69 @@
 // server.js
-const express = require('express')
-const cors=require('cors')
+const express = require("express");
+const cors = require("cors");
 // create server
-const app = express()
+const app = express();
 // import mongodb function
-const {User}=require('./model')
-app.use(cors())
+const { User } = require("./model");
+app.use(cors());
+//get request data from req.body
+app.use(express.json());
 const tokens = {
-    admin: {
-      token: 'admin-token'
+  admin: {
+    token: "admin-token",
+  },
+  editor: {
+    token: "editor-token",
+  },
+};
+const PREFIX = "/vue-admin-template/user";
+/* Login */
+app.post(`${PREFIX}/login`, async (req, res) => {
+  const token = tokens["admin"];
+  res.send({ code: 20000, data: token });
+});
+/* User info */
+app.get(`${PREFIX}/info`, async (req, res) => {
+  res.send({
+    code: 20000,
+    data: {
+      roles: ["admin"],
+      introduction: "I am a super administrator",
+      avatar:
+        "https://www.waikato.ac.nz/__data/assets/image/0003/661710/Vertical,-full-colour-logo,-on-white-background-1.jpg",
+      name: "Testing",
     },
-    editor: {
-      token: 'editor-token'
-    }
-  }
-  
-app.post('/vue-admin-template/user/login', async (req, res) => {
-    const token=tokens['admin']
-  res.send({code:20000,data:token})
-})
-app.get('/vue-admin-template/user/info', async (req, res) => {
+  });
+});
+/* Logout  */
+app.post(`${PREFIX}/logout`, async (req, res) => {
+  res.send({
+    code: 20000,
+    data: "success",
+  });
+});
+/* Register  */
+app.post(`${PREFIX}/register`, async (req, res) => {
+  const body = req.body;
+  const isDuplicate = await User.findOne({
+    username: body.username,
+  });
+  if (isDuplicate) {
     res.send({
-        "code": 20000,
-        "data": {
-            "roles": [
-                "admin"
-            ],
-            "introduction": "I am a super administrator",
-            "avatar": "https://www.waikato.ac.nz/__data/assets/image/0003/661710/Vertical,-full-colour-logo,-on-white-background-1.jpg",
-            "name": "Testing"
-        }
-    })
-  })
-app.post('/vue-admin-template/user/logout', async (req, res)=>{
-    res.send({
-        code:20000,
-        data:'success'
-    })
-})
-app.post('/vue-admin-template/user/register', async (req, res)=>{
-    const isDuplicate = await User.findOne({
-        username: "Not duplicate"
-      })
-    if (isDuplicate) {
+      code: 422,
+      message: "Duplicate User!",
+    });
+  } else {
+    User.create({ username: body.username, password: body.password }).then(
+      () => {
         res.send({
-            code:422,
-            message: 'Duplicate User!'
-        })
-    }else{
-        const user = await User.create({
-            username: "Not duplicate",
-            password: "12345"
-          })
-          res.send({
-            code:20000,
-            message:'Register Success',
-            data:user
-        })
-    }
-
-})
+          code: 20000,
+          message: "Register Success",
+        });
+      }
+    );
+  }
+});
 app.listen(9528, () => {
-  console.log('http://localhost:9528')
-})
-
+  console.log("http://localhost:9528");
+});
